4d07356db07b83bdce171d0777ffb1424f11ea93
Tarun Durbha <tarundurbha14@gmail.com>
2025-01-02 09:05:06 +0530

    Initial commit for new branch

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Navbar from "./Navbar.js"
+import Main from "./Main.js"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className="navbar"> 
+        <div>
+            Online Spreadsheet
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
